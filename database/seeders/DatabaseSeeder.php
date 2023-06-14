ddeecf02b4a53502cdceab1cc3d9ca55b7a68aaf
Mukhammad Mahdy <mahdy.mukhammad@gmail.com>
2023-06-14 12:30:47 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,6 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             UserSeeder::class,
             CategorySeeder::class,
-            BrandSeeder::class,
             ProductSeeder::class,
             SliderSeeder::class,
         ]);
