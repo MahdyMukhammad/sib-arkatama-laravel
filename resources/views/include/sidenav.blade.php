1313aa28015c36b6d571e94b5de58536197ecfb6
Mukhammad Mahdy <mahdy.mukhammad@gmail.com>
2023-06-22 13:51:40 +0700

    update

--- resources/views/include/sidenav.blade.php
+++ resources/views/include/sidenav.blade.php
@@ -35,8 +35,8 @@
                     </div>
                 @endif
 
-                <div class="sb-sidenav-menu-heading">MANAGEMENT</div>
                 @if (Auth::user()->role->name == 'Admin')
+                    <div class="sb-sidenav-menu-heading">MANAGEMENT</div>
                     <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
                         data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
                         <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
